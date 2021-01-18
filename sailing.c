383b321e430cc1e4d7afcb91df1ff7b7ed66c49c
chatziiola <chatziiol@gmail.com>
2021-01-19 00:20:01 +0200

    Menu completed

--- sailing.c
+++ sailing.c
@@ -8,19 +8,16 @@
  * MENU:
  * STDLIB: use of the exit() function TODO check whether it is necessary
  */
-#include <ncurses.h>
 #include <stdio.h>
 #include <menu.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ncurses.h>
 
-
-/*
- * Environment Variables
- */
-	
 // gameTITLE
 // - not much to say for this one -
-#define gameTITLE "MALAKIA"
+#define gameTITLE "Sailing-game"
+#define CTRLD 4
 
 // frameDURATION
 //		@usage:
@@ -31,78 +28,165 @@
 //		@usage:
 // 		@details:
 #define gameWIDTH 80
-#define gameHEIGHT 40
+#define gameHEIGHT 20
 
 // menuWIDTH, menuHEIGHT
 //		@usage:
 // 		@details:
 #define menuWIDTH 50
 #define menuHEIGHT 18
-#define menuXCOORD ( COLS - menuWIDTH )/2
-#define menuYCOORD ( LINES - menuHEIGHT )/2
 
-// landCHAR, landCOLOR
-// seaCHAR, seaCOLOR
-// boatCHAR, boatCOLOR
-//		@usage:
-// 		@details:
-#define landCHAR '*'
-#define seaCHAR '-'
-#define boatCHAR 'b'
-// We need to do some research on that
-// TODO associate it with COLOR_PAIR()
-#define landCOLOR COLOR_BLACK
-#define seaCOLOR COLOR_BLACK
-#define boatCOLOR COLOR_BLACK
+char *menuOptions[] = {"Play","Controls","Credits","Exit", (char *)NULL};
 
-
-/*
- * Environment Functions
- * (To make the code more readable)
+/* 
+ * printInMiddle:
+ * @params:
+ * takes as arguments a window in which the string will be printed
+ *  an integer row, the line in which we want the screen to be printed
+ * (if row<0, then the string will be printed |row| lines from the bottom 
+ * of the window), the string we want to print, and attributes attrs which in 
+ * most cases will be the color
  */
-#define arraySIZE(l) (sizeof(l) / sizeof(l[0]))
-
-
-/*
- * Global variables
- * 		(Even though this isn't the optimal way of doing things)
- */
-char *menuOptions[] = {"Play","Difficulty","Controls","Credits","Exit"};
-
-/*
- * Our structures
- */
-
-//typedef struct _random
-//{
-//}random;
-
-/*
- * Function Declarations
- */
-
-// TODO add description and complete the function
-void printInMiddle(WINDOW *win, int startY, int startX, int width, char *string, chtype color);
-
-/*
- * EXTRA FEATURES, consider adding
- *
- * Check whether we should use cbreak or raw
- * Enable the usage of the function keys: F1, F2, F...
- * keypad(stdscr, TRUE);
- *
- * These two lines should be followed by a comment explaining that COLS and LINES variables are set during initscr();
- * Serving as the dimension of the terminal
- * posX = ( COLS - menuWIDTH )/2;
- * posY = ( LINES - menuHEIGHT)/2;
- */
+void showMenu(char * answer);
+void initialization();
+void printInMiddle(WINDOW *win, int y, char *string, chtype color);
 
 int main()
 {
-	/*
-	 * Initialization
-	 */
+	char choice[10];
+
+	// Ncurses general settings 
+	initialization();	
+	// Show menu and save the user's choice in the choice char arr
+	showMenu(choice);	
+
+	if(strcmp(choice,"Exit") == 0)
+	{
+		endwin();
+		printf("%s\n","Thanks for playing our game");
+		return 1;
+	}
 	
+	// Releasing memory allocated during the creation of our menu
+	endwin();
+	return 1;
+}
+
+void printInMiddle(WINDOW *win, int row, char *string, chtype attrs)
+{
+	// These two variables will hold the dimensions of our window
+	int maxRows, maxCols;
+	// getmaxyx function simply gives us the maximum y and x for that window
+	getmaxyx(win, maxRows, maxCols);
+
+	// If the value of row is negative then the string will be printed
+	// |row| lines from the bottom of the window
+	if (row < 0)
+		row = maxRows -1 + row;
+
+	wattron(win, attrs);
+	// Print string in win after moving to y line and column () so that the
+	// string is placed in the middle of the window
+	mvwprintw(win, row, (maxCols-strlen(string))/2, string);
+	wattroff(win, attrs);
+}	
+
+
+void showMenu(char * answer)
+{
+	WINDOW *mainMenuWin;
+	MENU *mainMenu;
+	ITEM *playerChoice,
+		 *currentItem,
+		 **myItems;
+	int ch;
+	
+	// The number of options in our menu is equal to the number of elements
+	// in the menuOptions array, initialized globally
+	int noOfoptions = sizeof(menuOptions) / sizeof(menuOptions[0]);
+	// Memory for the items we want to create must be reserved:
+	// - the pointer myItems simply points to the beginning of our ITEM * array
+	// - the (ITEM **) is to turn the pointer returned by calloc into a ITEM**
+	// type of pointer
+	myItems = (ITEM **)calloc(noOfoptions, sizeof(ITEM *));
+
+	// Filling the array with our menu options, described in our menuOptions ar
+	// The second argument of the new_item function is the description
+	// of that item, displayed right next to the item. Since we want to 
+	// keep the interface minimal we decided against having a description.
+	// We supply a NULL pointer to have an empty description.
+	// The set_item_userptr() function is responsible for the function that
+	// runs when the specific item is selected
+	
+	for(int l = 0; l < noOfoptions; l++)
+		myItems[l] = new_item( menuOptions[l], NULL);
+	
+	//  We create a menu, with items all of our initial options, 
+	//  in the desired form
+	mainMenu = new_menu((ITEM **)myItems);
+	// Create the window for our menu
+	// it creates a window of menuHEIGHT by menuWIDTH dimensions and places
+	// it at the latter 2 args
+  	mainMenuWin = newwin(menuHEIGHT, menuWIDTH, (LINES-menuHEIGHT)/2, (COLS-menuWIDTH)/2);
+	// enable the usage of keyboard for our window
+	keypad(mainMenuWin, TRUE);
+	// Set main window and subwindow for our menu 
+	// (simply creating it would not be enough, the two of them must be
+	// linked together)
+	set_menu_win(mainMenu, mainMenuWin);
+	// The derwin() function is the same as subwin(), creating a virtual
+	// subwindow in the window supplemented, with the only difference being
+	// that the coordinates are relative to our window
+	// ( not to stdscr, the standard terminal screen )
+	set_menu_sub(mainMenu, derwin(mainMenuWin, menuHEIGHT-4, menuWIDTH-4, 3,1));
+	// Set which character should point at our current option:
+	set_menu_mark(mainMenu, " => ");
+	// Print a border for our menu and a title:
+	// 0,0 gives default characters for the vertical and horizontal lines
+	box( mainMenuWin, 0, 0 );
+	// Print the gameTITLE in the middle of the 1st line of our menu window 
+	printInMiddle(mainMenuWin, 1, gameTITLE, COLOR_PAIR(1));
+	mvwaddch(mainMenuWin, 2, 0, ACS_LTEE);
+    mvwhline(mainMenuWin, 2, 1, ACS_HLINE, menuWIDTH-2);
+    mvwaddch(mainMenuWin, 2, menuWIDTH-1, ACS_RTEE);
+	// We set the menu options: 
+	//  We make our menu visible, and initialize our 'choiceIsMade' boolean
+	//  to false
+	bool choiceIsMade = false;
+	printInMiddle(stdscr, -1, "F1 to exit", COLOR_PAIR(1)| A_BLINK);
+	refresh();
+
+	post_menu(mainMenu);
+	wrefresh(mainMenuWin);
+	
+	while(!choiceIsMade)
+	{
+		ch = wgetch(mainMenuWin);
+		switch(ch)
+		{
+			case KEY_DOWN:
+				menu_driver(mainMenu, REQ_DOWN_ITEM);
+				break;
+			case KEY_UP:
+				menu_driver(mainMenu, REQ_UP_ITEM);
+				break;
+			// The value 10 is associated with the enter key
+			case 10:
+				playerChoice = current_item(mainMenu);
+				choiceIsMade = true;
+		}
+		wrefresh(mainMenuWin);
+	}
+	unpost_menu(mainMenu);
+	free_menu(mainMenu);
+	strcpy(answer, item_name(playerChoice));
+}
+
+void initialization()
+{
+	// We start the curses mode ( enabling windows functionality )
+	initscr();
+
 	// Check whether the terminal has color integration
 	// -> TRUE: continue with the program
 	// -> FALSE: show error
@@ -114,8 +198,6 @@ int main()
 		exit(1);
 	}
 
-	// We start the curses mode ( enabling windows functionality )
-	initscr();
 	// Checking whether the terminal is of valid size ( if it is too small the
 	// program would, otherwise, malfunction
 	if ( (COLS < gameWIDTH) ||  (LINES < gameHEIGHT) )
@@ -128,102 +210,20 @@ int main()
 	}
 	// Disable line buffering so that we get each keypress in real time
 	// without waiting for a <NL> or <CR> character
-	raw();
+	// There was also the raw() option but this one was selected so that
+	// the "standard" keybindings would still function properly
+	cbreak();
 	// We don't want to see the characters as they're typed in:
 	// ( user's input )
 	noecho();
+	keypad(stdscr, TRUE);
 	// Make the cursor invisible
 	curs_set(0);
 	// Enable colors and set color pairs, see README FOR MORE
 	start_color();
-	// TODO You may want to check the associated values land/sea/boatCOLOR 
 	// to make everything more readable
 	// Also take into consideration the menu colors
 	init_pair(1, COLOR_RED, COLOR_BLACK);
 	init_pair(2, COLOR_GREEN, COLOR_BLACK);
 	init_pair(3, COLOR_BLUE, COLOR_BLACK);
-	
-	/*
-	 * Variables
-	 */
-	
-	// ch .... getch()
-	int ch,
-		posX,
-		posY;
-
-	/*
-	 * Menu initialization
-	 */	
-
-	// Related to our menu
-	WINDOW *menuWin;
-	MENU *mainMenu;
-	ITEM *currentItem,
-		 **myItems;
-	
-	// The number of options in our menu is equal to the number of elements
-	// in the menuOptions array, initialized globally
-	int noOfoptions = arraySIZE(menuOptions);
-	// Memory for the items we want to create must be reserved:
-	// - we reserve memory for noOfoptions + 1 items, since we want to add
-	// a NULL pointer at the end to signal the end of our array
-	// - the pointer myItems simply points to the beginning of our ITEM * array
-	// - TODO the (ITEM **) is to turn the pointer returned by calloc into a ITEM**
-	// type of pointer
-	myItems = (ITEM **)calloc(noOfoptions + 1, sizeof(ITEM *));
-	// Filling the array with our menu options, described in our menuOptions ar
-	for ( int l = 0; l < noOfoptions; l++ )
-		// The second argument of the new_item function is the description
-		// of that item, displayed right next to the item. Since we want to 
-		// keep the interface minimal we decided against having a description.
-		// We supply a NULL pointer to have an empty description.
-		myItems[l] = new_item( menuOptions[l], NULL );
-	// We need to have the last item as null so that the menu initialization
-	// stops at the end of the array
-	myItems[noOfoptions] = (ITEM *)NULL;
-	//  We create a menu, with items all of our initial options, 
-	//  in the desired form
-	mainMenu = new_menu((ITEM **)myItems);
-
-	// Create the window for our menu
-	// TODO add a better description
-  	menuWin = newwin(menuHEIGHT, menuWIDTH, menuXCOORD, menuYCOORD);
-	// Set main window and subwindow for our menu 
-	// (simply creating it would not be enough, the two of them must be
-	// linked together)
-	set_menu_win( mainMenu, menuWin );
-	// The derwin() function is the same as subwin(), creating a virtual
-	// subwindow in the window supplemented, with the only difference being
-	// that the coordinates are relative to our window
-	// ( not to stdscr, the standard terminal screen )
-	set_menu_sub( mainMenu, derwin( menuWin, 6, 38, 3, 1));
-	// TODO once again
- 	set_menu_format( mainMenu, 5, 1 );
-	// Set which character should point at our current option:
-	set_menu_mark( mainMenu, "=>");
-	// Print a border for our menu and a title:
-	// TODO comment the arguments
-	box( menuWin, 0, 0 );
-	printInMiddle( menuWin, 1, 0, menuWIDTH,gameTITLE, COLOR_PAIR(1) );
-	// We set the menu options: 
-	set_menu_fore( mainMenu, COLOR_PAIR(1) | A_REVERSE );
-	// set_menu_back, set_menu_grey
-	//  We make our menu visible
-	post_menu( mainMenu );
-	wrefresh( menuWin );
-	// TODO menu_scroll.c
-	
-	// Add this line at the end of the menu interface
-	// unpost_menu( mainMenu );
-	
-	/*
-	 * Main structure
-	 */
-	
-
-	// Releasing memory allocated during the creation of our menu
-	free_menu( mainMenu );
-	endwin();
-	return 1;
 }
